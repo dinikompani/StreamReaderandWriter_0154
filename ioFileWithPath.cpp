3606f595bfb7bea8990ada49cb013e589cb2322d
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-10 11:04:37 +0700

    Loop membaca isi file dan tampilkan

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,4 +30,21 @@ int main() {
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakuka perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+    
 }
