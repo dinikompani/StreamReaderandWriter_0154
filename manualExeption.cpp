e8164eb65c85dba6c3c4a089cd3dca78923ec1fb
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-10 11:15:08 +0700

    Mmebuat program dengan exception handling

---
+++ manualExeption.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+        
+    }
+
+
+}
