6a7d6fce0f23686ad9f0907b85bdd8e53f59e6c8
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-10 11:27:18 +0700

    Tambahkan catch untuk menangani exception

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -12,7 +12,14 @@ int main()
         //pesan array integer 3 elemen
         cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
+
     }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+    }
+    cout << "Baris Program yang terakhir" << endl;
+    return 0;
 }
 
 
