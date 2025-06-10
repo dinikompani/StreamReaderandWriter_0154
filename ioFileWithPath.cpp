febd11ec5499a8d52f3c07248ed9685d9c38d359
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-10 11:03:09 +0700

    Loop menulis isi file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,4 +10,22 @@ int main() {
     cout << "Masukkan Nama File : ";
     cin >> NamaFile;
 
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter 'q'
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    
 }
