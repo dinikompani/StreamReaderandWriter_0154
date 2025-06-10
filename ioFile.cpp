56e09503d41052b1a9e50d2213ed1160164dd99e
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-10 10:39:05 +0700

    Cek apakah file berhasil dibuka

--- ioFile.cpp
+++ ioFile.cpp
@@ -33,7 +33,11 @@ int main() {
     //menunjuk ke sebuah file
     infile.open("contohFile.txt");
 
-    
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
 
+    }
 
 }
